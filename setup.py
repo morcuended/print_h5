7cdf557953f735c1970a23d1286f910f09ebe2f2
Daniel Morcuende <dmorcuen@ucm.es>
2018-06-04 18:18:26 +0200

    installing with setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+requirements = ['h5py']
+setup(
+    name='print_h5',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Daniel Morcuende',
+    author_email='dmorcuen@ucm.es',
+    description=''
+)
